9e078b0c13663c5207c0835b48412c1f5f456ae6
Mohamad El-Husseini <husseini.mel@gmail.com>
2014-04-08 00:01:09 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# libpq-dev cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Mohamad El-Husseini
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'libpq-dev'
+maintainer       'Mohamad El-Husseini'
+maintainer_email 'husseini.mel@gmail.com'
+license          'Apache 2'
+description      'Installs/Configures libpq-dev'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+

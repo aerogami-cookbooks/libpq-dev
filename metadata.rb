7c3ac602557522d0b5c27269eba7c5428ad3ed7d
Mohamad El-Husseini <husseini.mel@gmail.com>
2014-04-09 12:30:26 -0300

    updated recipe readme and definitions

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,12 @@ name             'libpq-dev'
 maintainer       'Mohamad El-Husseini'
 maintainer_email 'husseini.mel@gmail.com'
 license          'Apache 2'
-description      'Installs/Configures libpq-dev'
+description      'Installs libpq-dev'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+recipe 'libpq-dev', 'Installs libpq-dev'
+
+supports 'Ubuntu'
+
+depends 'apt'
